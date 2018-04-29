69479b54941db2be1a2a58fc319e035e17bdc4cd
Zach <zachary.lynch1337@gmail.com>
2018-04-28 16:49:19 -0800

    Parsed JSON to populate the Sandwich object. Got all the parsing to work except the alsoKnownAs array.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,46 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    private static final String TAG = JsonUtils.class.getSimpleName();
+
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+
+        JSONObject swData = new JSONObject(json);
+        JSONObject swName = swData.getJSONObject("name");
+        Sandwich sandwich = new Sandwich();
+        sandwich.setMainName(swName.getString("mainName"));
+        Log.v(TAG, "From JSON " + sandwich.getMainName());
+        sandwich.setPlaceOfOrigin(swData.getString("placeOfOrigin"));
+        sandwich.setDescription(swData.getString("description"));
+        sandwich.setImage(swData.getString("image"));
+        sandwich.setIngredients(getIngredients(json));
+       Log.v(TAG, "From JSON " + sandwich.getDescription());
+       return sandwich;
     }
+
+    private static List<String> getIngredients(String json) throws JSONException {
+        ArrayList<String> ingredientsData = new ArrayList<>();
+        JSONObject ingObj = new JSONObject(json);
+        JSONArray ingredients = ingObj.getJSONArray("ingredients");
+        for (int i = 0; i < ingredients.length(); i++){
+            ingredientsData.add(ingredients.getString(i));
+        }
+        return ingredientsData;
+
+    }
+
+
 }
